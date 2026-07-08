4df8aeffa7b45de146bd53a4e04da0a452149281
Nathan <an.email.com>
2026-07-08 15:47:19 +0100

    add ticker helper

--- src/ticker.rs
+++ src/ticker.rs
@@ -3,6 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Ticker(pub String);
 
+impl From<&str> for Ticker {
+    fn from(value: &str) -> Self {
+        Ticker(value.into())
+    }
+}
+
 impl std::fmt::Display for Ticker {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
