3722762029591367f3a851bccf3feb55447d717f
Nathan <an.email.com>
2026-08-13 11:44:30 +0100

    add extra info

--- src/binance/exchange_info.rs
+++ src/binance/exchange_info.rs
@@ -1,4 +1,5 @@
 use crate::{binance::rate_limits::BinanceRateLimit, ticker::Ticker};
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 use strum::Display;
 
@@ -22,10 +23,25 @@ pub enum BinanceExchangeInfoSymbolStatus {
 #[allow(non_snake_case, unused)]
 #[derive(Debug, Clone, Deserialize)]
 pub struct BinanceExchangeInfoResult {
-    pub timezone: String,
-    pub serverTime: i64,
+    pub exchangeFilters: Vec<BinanceExchangeFilter>,
     pub rateLimits: Vec<BinanceRateLimit>,
+    pub serverTime: i64,
     pub symbols: Vec<BinanceExchangeInfoSymbol>,
+    pub timezone: String,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(tag = "filterType")]
+pub enum BinanceExchangeFilter {
+    EXCHANGE_MAX_NUM_ORDERS {
+        max_num_orders: i64,
+    },
+    EXCHANGE_MAX_NUM_ALGO_ORDERS {
+        max_num_algo_orders: i64,
+    },
+    #[serde(other)]
+    Unknown,
 }
 
 #[allow(non_snake_case, unused)]
@@ -34,6 +50,7 @@ pub struct BinanceExchangeInfoSymbol {
     pub baseAsset: Ticker,
     pub baseAssetPrecision: u8,
     pub baseCommissionPrecision: u8,
+    pub filters: Vec<BinanceFilter>,
     pub isSpotTradingAllowed: bool,
     pub orderTypes: Vec<BinanceOrderType>,
     pub quoteAsset: Ticker,
@@ -55,4 +72,58 @@ pub enum BinanceOrderType {
     STOP_LOSS_LIMIT,
     TAKE_PROFIT,
     TAKE_PROFIT_LIMIT,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(tag = "filterType")]
+pub enum BinanceFilter {
+    PRICE_FILTER {
+        min_price: Decimal,
+        max_price: Decimal,
+        tick_size: Decimal,
+    },
+    /// for limit orders
+    LOT_SIZE {
+        min_qty: Decimal,
+        max_qty: Decimal,
+        step_size: Decimal,
+    },
+    /// for market orders, if absent LOT_SIZE is used
+    MARKET_LOT_SIZE {
+        min_qty: Decimal,
+        max_qty: Decimal,
+        step_size: Decimal,
+    },
+    MIN_NOTIONAL {
+        min_notional: Decimal,
+        #[serde(default)]
+        apply_min_to_market: Option<bool>,
+        #[serde(default)]
+        avg_price_mins: Option<i64>,
+    },
+    MAX_NUM_ORDERS {
+        max_num_orders: i64,
+    },
+    MAX_NUM_ALGO_ORDERS {
+        max_num_algo_orders: i64,
+    },
+    MAX_NUM_ICEBERG_ORDERS {
+        max_num_iceberg_orders: i64,
+    },
+    ICEBERG_PARTS {
+        limit: i64,
+    },
+    PERCENT_PRICE {
+        multiplier_up: Decimal,
+        multiplier_down: Decimal,
+        avg_price_mins: i64,
+    },
+    TRAILING_DELTA {
+        min_trailing_delta: Decimal,
+        max_trailing_delta: Decimal,
+        avg_price_mins: i64,
+    },
+    #[serde(other)]
+    Unknown,
 }
