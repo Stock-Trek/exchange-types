41ecfb44d08f8e142b06c6c53a9b55375cece3c2
Nathan J Adams <nathanjadams1@gmail.com>
2026-09-03 11:05:10 +0100

    use lower level trait (#51)
    Co-authored-by: Nathan <an.email.com>

--- src/http.rs
+++ src/http.rs
@@ -1,13 +1,16 @@
-pub trait IntoHttpRequest {
-    fn into_http_request(self) -> HttpRequest;
-}
-
 #[derive(Debug, Clone)]
 pub struct HttpRequest {
     pub method: HttpMethod,
     pub query: Option<String>,
     pub headers: Vec<(String, String)>,
     pub body: Option<Vec<u8>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct HttpResponse {
+    pub status: u16,
+    pub headers: Vec<(String, String)>,
+    pub body: Vec<u8>,
 }
 
 #[derive(Debug, Clone, Copy)]
